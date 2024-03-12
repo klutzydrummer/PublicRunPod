7c10462b4c592357fcc6ed62db70f08a6f43123b
klutzydrummer <45779155+klutzydrummer@users.noreply.github.com>
2024-03-12 15:03:18 -0500

    Update comfy_ui_pre-provision.sh

--- comfy_ui_pre-provision.sh
+++ comfy_ui_pre-provision.sh
@@ -1,5 +1,5 @@
-cd /workspace
+cd /opt
 git clone https://$GITHUB_TOKEN@github.com/klutzydrummer/runpod-provisioning-scripts.git
-cd /workspace/runpod-provisioning-scripts
+cd /opt/runpod-provisioning-scripts
 max_connections=16
 source ./comfy_ui_provision.sh
